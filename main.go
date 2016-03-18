a426f52cb2eb54644628cea3100497fdba79e091
Arjan Molenaar <gaphor@gmail.com>
2016-03-18 15:28:00 +0100

    Add random number to differentiate nodes.

--- main.go
+++ main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+        "math/rand"
 )
 
+var number int
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Oops2 %s!", r.URL.Path[1:])
+	fmt.Fprintf(w, "Oops %d %s!", number, r.URL.Path[1:])
 }
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	number = rand.Intn(1000)
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
